3ff1998a417d5cb673ea84db4118658500e74ae6
takaya221 <221.takaya.n@gmail.com>
2022-02-05 00:59:09 +0000

    08_4

--- app/Post.php
+++ app/Post.php
@@ -6,11 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    
+    protected $fillable = [
+    'title',
+    'body',
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 2)
     {
         // updated_atで降順にして、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
 }
 
 
